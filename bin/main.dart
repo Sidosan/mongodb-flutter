e96bedd60068abb5c1a7f46cba953826222ca906
Sidosan <filipe.sidosan@gmail.com>
2020-11-05 11:20:36 -0300

    main

--- bin/main.dart
+++ bin/main.dart
@@ -3,16 +3,12 @@ import 'package:mongo_dart/mongo_dart.dart';
 main(List<String> arguments) async {
   Db db = Db('mongodb://admin:admin@localhost:27017/test');
 
-  ObjectId id;
   DbCollection coll;
 
   await db.open();
-  print('connection open');
-
-  coll = db.collection('count');
-
-  var val = await coll.findOne(-1);
+  coll = db.collection("count");
+  print('conexao realizada com Banco $coll');
+  var val = await coll.findOne({'_id': 'total'});
   print('$val');
-
   await db.close();
 }
